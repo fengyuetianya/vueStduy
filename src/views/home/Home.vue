869253beb6adfc97ab65f7b56ac95decf11dfcea
fengyuetianya <380385129@qq.com>
2021-02-22 11:09:50 +0800

    git add .

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,76 @@
 <template>
-    <h2>主页</h2>
+<div id="home">
+   <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+   <swiper>
+      <swiper-item v-for="item in banners" :key="item.link">
+        <a :href="item.link">
+            <img :src="item.image" alt="">
+        </a>
+      </swiper-item>
+   </swiper>  
+</div>           
 </template>
 
 <script>
+import NavBar from '@/components/common/navbar/NavBar'
+import {Swiper,SwiperItem} from '@/components/common/swiper'
+import {getMultiData} from "@/network/home";
+
+
 export default {
-    name: 'Home'
+    name: 'Home',
+    data() {
+      return {
+        banners: [],
+        recommends: []
+      }
+    },
+    components: {
+      NavBar,
+      Swiper,
+      SwiperItem
+    },
+   
+    created() {
+      this._getMultiData()
+    },
+    methods: {
+      _getMultiData(){
+        getMultiData().then(res => {
+          console.log(res.data.data.banner.list);
+          this.banners = res.data.data.banner.list;
+          this.recommends = res.data.data.recommend.list;
+        })
+      }
+    }
 }
+ 
 </script>
 
 
-<style>
+<style scoped>
+  #home {
+    height: 100vh;
+    position: relative;
+  }
+
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #fff;
+    position: relative;
+    z-index: 9;
+  }
+
+  .tab-control {
+    position: relative;
+    z-index: 9;
+  }
+
+  .content {
+    position: absolute;
+    top: 44px;
+    bottom: 49px;
+    left: 0;
+    right: 0;
+  }
 </style>
